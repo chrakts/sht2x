57fda8de2ac1222906362a8e5f7757a1e9fadc0f
Christof <chrak@cmja.de>
2019-05-12 16:16:28 +0200

    Erstausgabe

---
+++ sht2x.cpp
@@ -0,0 +1,277 @@
+#include "sht2x.h"
+
+SHT2::SHT2(void)
+{
+}
+
+SHT2::SHT2(TWI_MasterDriver_t *mytwi,uint8_t address)
+{
+  twi = mytwi;
+  twiAdress = address;
+}
+
+SHT2::~SHT2()
+{
+  //dtor
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::write(uint8_t* data, uint8_t num)
+{
+   TWI_MasterWrite(twi,twiAdress,data,num);
+   while (twi->status != TWIM_STATUS_READY) {
+		/* Wait until transaction is complete. */
+	 }
+	 return(twi->result);
+}
+
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::read(uint8_t* data, uint8_t num)
+{
+uint8_t i;
+  TWI_MasterRead(twi,twiAdress,num);
+  while (twi->status != TWIM_STATUS_READY) {
+	/* Wait until transaction is complete. */
+	}
+	for(i=0;i<num;i++)
+    data[i] = twi->readData[i];
+  return(twi->result);
+}
+
+uint8_t SHT2::writeRead(uint8_t* writeData, uint8_t writeNum, uint8_t* readData, uint8_t readNum)
+{
+uint8_t i;
+  TWI_MasterWriteRead(twi,twiAdress,writeData,writeNum,readNum);
+  while (twi->status != TWIM_STATUS_READY) {
+  /* Wait until transaction is complete. */
+  }
+	for(i=0;i<readNum;i++)
+    readData[i] = twi->readData[i];
+  return(twi->result);
+}
+/*! \brief calculates checksum
+ *
+ *  calculates checksum for n bytes of data and compares
+ *  it with expected checksum
+ *
+ *  \param data[] checksum is built based on this data
+ *  \param nbrOfBytes   checksum is built for n bytes of data
+ *  \param checksum     expected checksum
+ *
+ *  \retval CHECKSUM_ERROR  checksum does not match
+ *  \retval 0 checksum matches
+ */
+uint8_t SHT2::CheckCrc(uint8_t data[], uint8_t nbrOfBytes, uint8_t checksum)
+{
+  uint8_t crc = 0;
+  uint8_t byteCtr;
+  //calculates 8-Bit checksum with given polynomial
+  for (byteCtr = 0; byteCtr < nbrOfBytes; ++byteCtr)
+  {
+    crc ^= (data[byteCtr]);
+    for (uint8_t bit = 8; bit > 0; --bit)
+    {
+      if (crc & 0x80) crc = (crc << 1) ^ POLYNOMIAL;
+      else crc = (crc << 1);
+    }
+  }
+  if (crc != checksum) return CHECKSUM_ERROR;
+  else return 0;
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::ReadUserRegister(uint8_t* pRegisterValue)
+{
+  uint8_t error=0;    //variable for error code
+  uint8_t command;
+  uint8_t answer[2];
+
+  command = USER_REG_R;
+  error = write(&command,1);
+  read(answer,2);
+  error += CheckCrc (&answer[0],1,answer[1]);
+  return error;
+
+/*  I2c_StartCondition();
+  error |= I2c_WriteByte (I2C_ADR_W);
+  error |= I2c_WriteByte (USER_REG_R);
+  I2c_StartCondition();
+  error |= I2c_WriteByte (I2C_ADR_R);
+  *pRegisterValue = I2c_ReadByte(ACK);
+  checksum=I2c_ReadByte(NO_ACK);
+  error |= SHT2x_CheckCrc (pRegisterValue,1,checksum);
+  I2c_StopCondition();
+  return error;*/
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::WriteUserRegister(uint8_t pRegisterValue)
+{
+  uint8_t command[2];
+  command[0] = USER_REG_W;
+  command[1] = pRegisterValue;
+  return write(command,2);
+
+/*
+  u8t error=0;   //variable for error code
+
+  I2c_StartCondition();
+  error |= I2c_WriteByte (I2C_ADR_W);
+  error |= I2c_WriteByte (USER_REG_W);
+  error |= I2c_WriteByte (*pRegisterValue);
+  I2c_StopCondition();
+  return error;
+*/
+}
+
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::getSerialNumber(uint8_t u8SerialNumber[])
+{
+  uint8_t commandData[2] = {0xFA,0x0F};
+  uint8_t readData[8];
+  uint8_t  error=0;                          //error variable
+
+  error = writeRead(commandData,2,readData,8);
+  u8SerialNumber[5] = readData[0];
+  u8SerialNumber[4] = readData[2];
+  u8SerialNumber[3] = readData[4];
+  u8SerialNumber[2] = readData[6];
+  commandData[0] = 0xFC;
+  commandData[0] = 0xC9;
+  error |= writeRead(commandData,2,readData,6);
+  u8SerialNumber[1] = readData[0];
+  u8SerialNumber[0] = readData[1];
+  u8SerialNumber[7] = readData[3];
+  u8SerialNumber[6] = readData[4];
+
+  return error;
+
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+float SHT2::CalcTemperatureC(uint16_t u16sT)
+{
+  float temperatureC;            // variable for result
+
+  u16sT &= ~0x0003;           // clear bits [1..0] (status bits)
+
+  //-- calculate temperature [°C] --
+  temperatureC= -46.85 + 175.72/65536 *(float)u16sT; //T= -46.85 + 175.72 * ST/2^16
+  return temperatureC;
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+float SHT2::CalcRH(uint16_t u16sRH)
+{
+float humidityRH;              // variable for result
+
+  u16sRH &= ~0x0003;          // clear bits [1..0] (status bits)
+  //-- calculate relative humidity [%RH] --
+  humidityRH = -6.0 + 125.0/65536 * (float)u16sRH; // RH= -6 + 125 * SRH/2^16
+  return humidityRH;
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::SoftReset()
+{
+  uint8_t command,error;
+  command = SOFT_RESET;
+  error = write(&command,1);
+  _delay_ms(15);
+  return(error);
+
+  /*
+  u8t  error=0;           //error variable
+  I2c_StartCondition();
+  error |= I2c_WriteByte (I2C_ADR_W); // I2C Adr
+  error |= I2c_WriteByte (SOFT_RESET);                            // Command
+  I2c_StopCondition();
+
+  DelayMicroSeconds(15000); // wait till sensor has restarted
+
+  return error;
+  */
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::MeasureHM(etSHT2xMeasureType eSHT2xMeasureType, int16_t* pMeasurand)
+{
+
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::startMeasurementPoll(etSHT2xMeasureType eSHT2xMeasureType)
+{
+  uint8_t  data;    //data array for checksum verification
+  uint8_t  error=0;    //error variable
+
+  switch(eSHT2xMeasureType)
+  { case  HUMIDITY: data = TRIG_RH_MEASUREMENT_POLL; break;
+    case TEMP     : data = TRIG_T_MEASUREMENT_POLL; break;
+    default: ;
+  }
+  error = write(&data,1);
+  return(error);
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+uint8_t SHT2::getMeasurementPoll(int16_t* pMeasurand)
+{
+  uint8_t  data[3];    //data array for checksum verification
+  uint8_t  error=0;    //error variable
+
+  error = write(NULL,0);
+  switch(error)
+  {
+    case TWIM_RESULT_NACK_RECEIVED:
+      return(0);
+    break;
+    case TWIM_RESULT_OK:
+      read(data,3);
+      *pMeasurand = data[0];
+      *pMeasurand = *pMeasurand<<8;
+      *pMeasurand += data[1];
+      error |= CheckCrc (data,2,data[2]);
+      return(error);
+    break;
+    default:
+      return(0);
+    break;
+  }
+}
+
---
+++ sht2x.h
@@ -0,0 +1,86 @@
+#ifndef SHT2_H
+#define SHT2_H
+
+#include <stdint.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <util/delay.h>
+#include "twi_master_driver.h"
+
+
+class SHT2
+{
+  public:
+    #define CHECKSUM_ERROR 0x80
+    typedef enum{
+    TRIG_T_MEASUREMENT_HM    = 0xE3, // command trig. temp meas. hold master
+    TRIG_RH_MEASUREMENT_HM   = 0xE5, // command trig. humidity meas. hold master
+    TRIG_T_MEASUREMENT_POLL  = 0xF3, // command trig. temp meas. no hold master
+    TRIG_RH_MEASUREMENT_POLL = 0xF5, // command trig. humidity meas. no hold master
+    USER_REG_W               = 0xE6, // command writing user register
+    USER_REG_R               = 0xE7, // command reading user register
+    SOFT_RESET               = 0xFE  // command soft reset
+    }etSHT2xCommand;
+
+    typedef enum {
+      SHT2x_RES_12_14BIT       = 0x00, // RH=12bit, T=14bit
+      SHT2x_RES_8_12BIT        = 0x01, // RH= 8bit, T=12bit
+      SHT2x_RES_10_13BIT       = 0x80, // RH=10bit, T=13bit
+      SHT2x_RES_11_11BIT       = 0x81, // RH=11bit, T=11bit
+      SHT2x_RES_MASK           = 0x81  // Mask for res. bits (7,0) in user reg.
+    } etSHT2xResolution;
+
+    typedef enum {
+      SHT2x_EOB_ON             = 0x40, // end of battery
+      SHT2x_EOB_MASK           = 0x40, // Mask for EOB bit(6) in user reg.
+    } etSHT2xEob;
+
+    typedef enum {
+      SHT2x_HEATER_ON          = 0x04, // heater on
+      SHT2x_HEATER_OFF         = 0x00, // heater off
+      SHT2x_HEATER_MASK        = 0x04, // Mask for Heater bit(2) in user reg.
+    } etSHT2xHeater;
+
+    // measurement signal selection
+    typedef enum{
+      HUMIDITY,
+      TEMP
+    }etSHT2xMeasureType;
+
+    typedef enum{
+      I2C_ADR_W                = 128,   // sensor I2C address + write bit
+      I2C_ADR_R                = 129    // sensor I2C address + read bit
+    }etI2cHeader;
+
+
+    SHT2(void);
+    SHT2(TWI_MasterDriver_t *mytwi,uint8_t address);
+    ~SHT2();
+    uint8_t write(uint8_t *data,uint8_t num);
+    uint8_t read(uint8_t* data, uint8_t num);
+    uint8_t writeRead(uint8_t* writeData, uint8_t writeNum, uint8_t* readData, uint8_t readNum);
+    uint8_t CheckCrc(uint8_t data[], uint8_t nbrOfBytes, uint8_t checksum);
+    uint8_t ReadUserRegister(uint8_t *pRegisterValue);
+    uint8_t WriteUserRegister(uint8_t pRegisterValue);
+    uint8_t startMeasurementPoll(etSHT2xMeasureType eSHT2xMeasureType);
+    uint8_t getMeasurementPoll(int16_t* pMeasurand);
+    uint8_t MeasureHM(etSHT2xMeasureType eSHT2xMeasureType, int16_t *pMeasurand);
+    uint8_t SoftReset();
+    float CalcRH(uint16_t u16sRH);
+    float CalcTemperatureC(uint16_t u16sT);
+    uint8_t getSerialNumber(uint8_t u8SerialNumber[]);
+
+  protected:
+
+  private:
+    uint8_t twiAdress;
+    TWI_MasterDriver_t *twi;
+    const uint16_t POLYNOMIAL = 0x131;  //P(x)=x^8+x^5+x^4+1 = 100110001
+    SHT2( const SHT2 &c );
+    SHT2& operator=( const SHT2 &c );
+
+};
+
+#endif // SHT2_H
